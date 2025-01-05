f782c83f2c68c671d34e3bc1bd4668e994669c56
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-04 22:03:56 -0500

    Gestión básica de clase y objeto

---
+++ 01_Car/src/main/java/Auto.java
@@ -0,0 +1,11 @@
+// Clase auto
+public class Auto {
+    // Atributos
+    String color;
+    String marca;
+
+    // Método void que imprime un mensaje en consola
+    void velocidad() {
+        System.out.println("velocidad del auto.");
+    }
+}
---
+++ 01_Car/src/main/java/Main.java
@@ -0,0 +1,10 @@
+// Clase Main
+public class Main {
+
+    // Definición de la clase Main
+    public static void main(String[] args) {
+        // Crear un nuevo objeto de la clase Auto llamado Toyota
+        Auto toyota = new Auto();
+        toyota.velocidad();
+    }
+}
