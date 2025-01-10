bce33e125140537a69c982c5a3bd39be6e8062a1
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-10 09:17:39 -0500

    Ejercicio con clases con más atributos y métodos, complementando lo realizado

--- 02_Devices/src/main/java/com/entrecodigosycafe/clases/Main.java
+++ 02_Devices/src/main/java/com/entrecodigosycafe/clases/Main.java
@@ -5,5 +5,9 @@ public class Main {
         Device Computadora = new Device("Computadora", "Asus", "Gris");
         System.out.println("El dispositivo es " + Computadora.getNombre() + " la marca es " + Computadora.getMarca() + " el color es " + Computadora.getColor());
         Computadora.promocion();
+
+        Device Monitor = new Device("Monitor", "Samsung", "Negro");
+        System.out.println("El dispositivo es " + Monitor.getNombre() + " la marca es " + Monitor.getMarca());
+        Monitor.promocion();
     }
 }
