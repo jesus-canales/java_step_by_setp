a56ac40eb2ff74c2c23c4cb32c756dfa999c5861
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-15 16:57:03 -0500

    Hemos agregado la clase Operadores Relacionales y empezamos con el operador Relacional

--- 04_operators/src/main/java/com/entrecodigosycafe/operators/Main.java
+++ 04_operators/src/main/java/com/entrecodigosycafe/operators/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("=== Operadores Aritméticos ===");
         OperadoresAritmeticos.ejecutar();
+
+        System.out.println("=== Operadores Relacionales ===");
+        OperadoresRelacionales.ejecutar();
     }
 }
---
+++ 04_operators/src/main/java/com/entrecodigosycafe/operators/OperadoresRelacionales.java
@@ -0,0 +1,12 @@
+package com.entrecodigosycafe.operators;
+
+public class OperadoresRelacionales {
+
+    public static void ejecutar () {
+        int valorA = 5;
+        int valorB = 8;
+
+        // Igual a
+        System.out.println(" ¿a == b? " + (valorA == valorB));
+    }
+}
