87a9b06f57cc15e811e44317e8e7c8fdafd30086
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-15 16:33:54 -0500

    Hemos creado el proyecto y agregado la clase operadores aritméticos y se empezó con la suma

---
+++ 04_operators/src/main/java/com/entrecodigosycafe/operators/Main.java
@@ -0,0 +1,9 @@
+package com.entrecodigosycafe.operators;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("=== Operadores Aritméticos ===");
+        OperadoresAritmeticos.ejecutar();
+    }
+}
---
+++ 04_operators/src/main/java/com/entrecodigosycafe/operators/OperadoresAritmeticos.java
@@ -0,0 +1,13 @@
+package com.entrecodigosycafe.operators;
+
+public class OperadoresAritmeticos {
+
+    public static void ejecutar () {
+
+        // Suma
+        int suma = 5 + 8;
+        System.out.println("La suma de ( 5 + 8 ) es: " + suma);
+
+
+    }
+}
