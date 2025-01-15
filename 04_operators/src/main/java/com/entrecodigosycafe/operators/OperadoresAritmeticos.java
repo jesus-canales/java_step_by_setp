2b7c15a2032b2ab31e87327bf3bb76a2a8fce7d8
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-15 16:43:04 -0500

    Hemos agregado ejemplo de operadores aritméticos

--- 04_operators/src/main/java/com/entrecodigosycafe/operators/OperadoresAritmeticos.java
+++ 04_operators/src/main/java/com/entrecodigosycafe/operators/OperadoresAritmeticos.java
@@ -8,6 +8,20 @@ public class OperadoresAritmeticos {
         int suma = 5 + 8;
         System.out.println("La suma de ( 5 + 8 ) es: " + suma);
 
+        // Resta
+        int resta = 10 - 3;
+        System.out.println("La resta de ( 10 - 3 ) es: " + resta);
 
+        // Multiplicación
+        int producto = 7 * 3;
+        System.out.println("La multiplicación de ( 7 * 3 ) es: " + producto);
+
+        // División
+        double division = 20.0 / 3;
+        System.out.println("La división de ( 20.0 / 3 ) es: " + division);
+
+        // Residuo
+        int residuo = 7 % 3;
+        System.out.println("El residuo de ( 7 % 3 ) es: " + residuo);
     }
 }
