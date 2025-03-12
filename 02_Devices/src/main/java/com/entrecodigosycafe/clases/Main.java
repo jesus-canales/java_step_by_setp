a2e14e8383e0850bc495a69fefa5406f1fd47e06
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-03-12 07:52:09 -0500

    Actualizado

--- 02_Devices/src/main/java/com/entrecodigosycafe/clases/Main.java
+++ 02_Devices/src/main/java/com/entrecodigosycafe/clases/Main.java
@@ -3,7 +3,7 @@ package com.entrecodigosycafe.clases;
 public class Main {
     public static void main(String[] args) {
         Device Computadora = new Device("Computadora", "Asus", "Gris");
-        System.out.println("El dispositivo es " + Computadora.getNombre() + " la marca es " + Computadora.getMarca() + " el color es " + Computadora.getColor());
+        System.out.println("El dispositivo es " + Computadora.getNombre() + ", la marca es " + Computadora.getMarca() + " el color es " + Computadora.getColor());
         Computadora.promocion();
 
         Device Monitor = new Device("Monitor", "Samsung", "Negro");
